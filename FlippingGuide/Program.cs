436f92763dce899a8b2d0ae1fa6ea7e260d8ea8c
bVelliquette <BenjaminVelliquette@gmail.com>
2020-07-16 17:27:38 -0400

    Add project files.

---
+++ FlippingGuide/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FlippingGuide
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
